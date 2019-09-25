2de0f31fefbb0f3d4abf2358c13c49a83e16238c
Miran Jang <miranjang9507@gmail.com>
2019-09-25 17:23:57 -0400

    basic code added

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,9 @@
+var friendsArr = [
+    {
+        "name" : "Dr.Seuss",
+        "photo": "https://i.kinja-img.com/gawker-media/image/upload/s--vLoAq8to--/c_scale,dpr_2.0,f_auto,fl_progressive,q_80,w_800/eu44csq2zirexpl26zk7.jpg",
+        "scores": [
+            5, 2, 5, 2, 3, 1, 5, 3, 2, 5
+        ]
+    }
+]
