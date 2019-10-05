5f7d055b3173b69bd9941c859253bd6953679327
Miran Jang <miranjang9507@gmail.com>
2019-10-05 19:02:03 -0400

    modal is fully functioning finally

--- FriendFinder/app/data/friends.js
+++ FriendFinder/app/data/friends.js
@@ -1,7 +1,7 @@
 var friendsArr = [
     {
         "name" : "Penny",
-        "photo": "https://i.kinja-img.com/gawker-media/image/upload/s--vLoAq8to--/c_scale,dpr_2.0,f_auto,fl_progressive,q_80,w_800/eu44csq2zirexpl26zk7.jpg",
+        "photo": "https://vignette.wikia.nocookie.net/bigbangtheory/images/4/4d/TCCO-6.jpg",
         "scores": [
             5, 4, 5, 2, 5, 2, 5, 1, 2, 5
         ]
@@ -9,7 +9,7 @@ var friendsArr = [
 
     {
         "name": "Leonard Hofstadter",
-        "photo": "",
+        "photo": "https://upload.wikimedia.org/wikipedia/en/thumb/7/77/Leonard_Hofstadter.jpg/220px-Leonard_Hofstadter.jpg",
         "scores": [
             2, 3, 2, 3, 1, 4, 3, 5, 1, 4
         ]
@@ -17,7 +17,7 @@ var friendsArr = [
 
     {
         "name": "Howard Wolowitz",
-        "photo": "",
+        "photo": "https://upload.wikimedia.org/wikipedia/en/f/fd/Howard-wolowitz-the-big-bang-theory-16865313-930-1246.jpg",
         "scores": [
             5, 4, 1, 2, 2, 4, 2, 3, 3, 3
         ]
@@ -25,7 +25,7 @@ var friendsArr = [
 
     {
         "name": "Raj Koothrappali",
-        "photo": "",
+        "photo": "https://vignette.wikia.nocookie.net/bigbangtheory/images/6/6b/Matcon56.jpg",
         "scores": [
             3, 5, 1, 3, 2, 2, 3, 4, 4, 5
         ]
@@ -33,7 +33,7 @@ var friendsArr = [
 
     {
         "name": "Sheldon Cooper",
-        "photo": "",
+        "photo": "https://the-big-bang-theory.com/images/uploads/5/rs_3463ec66edb459bd1dd.jpg",
         "scores": [
             1, 2, 1, 3, 1, 1, 1, 5, 5, 1
         ]
@@ -41,7 +41,7 @@ var friendsArr = [
 
     {
         "name": "Amy Farrah Fowler",
-        "photo": "",
+        "photo": "https://vignette.wikia.nocookie.net/bigbangtheory/images/e/e8/AmyHappy.jpg",
         "scores": [
             3, 3, 1, 2, 5, 4, 1, 5, 3, 2
         ]
@@ -49,7 +49,7 @@ var friendsArr = [
 
     {
         "name": "Bernadette Rostenkowski",
-        "photo": "",
+        "photo": "https://vignette.wikia.nocookie.net/warner-bros-entertainment/images/a/a6/Melissa-rauch-the-big-bang-theory-bernadette-tv-2016-vertical.jpg",
         "scores": [
             3, 1, 2, 1, 3, 1, 3,  5, 5, 1
         ]
