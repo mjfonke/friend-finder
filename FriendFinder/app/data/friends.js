197bfed9e8f3b6ba0991fbd1f9cafd3617ab04d3
Miran Jang <miranjang9507@gmail.com>
2019-10-04 21:36:00 -0400

    fixed bug on app.post

--- FriendFinder/app/data/friends.js
+++ FriendFinder/app/data/friends.js
@@ -3,15 +3,55 @@ var friendsArr = [
         "name" : "Penny",
         "photo": "https://i.kinja-img.com/gawker-media/image/upload/s--vLoAq8to--/c_scale,dpr_2.0,f_auto,fl_progressive,q_80,w_800/eu44csq2zirexpl26zk7.jpg",
         "scores": [
-            5, 2, 5, 2, 3, 1, 5, 3, 2, 5
+            5, 4, 5, 2, 5, 2, 5, 1, 2, 5
         ]
     },
 
     {
-        "name": "Leonard",
+        "name": "Leonard Hofstadter",
         "photo": "",
         "scores": [
-            4, 3, 5, 3, 1, 2, 5, 3, 4, 2
+            2, 3, 2, 3, 1, 4, 3, 5, 1, 4
+        ]
+    },
+
+    {
+        "name": "Howard Wolowitz",
+        "photo": "",
+        "scores": [
+            5, 4, 1, 2, 2, 4, 2, 3, 3, 3
+        ]
+    },
+
+    {
+        "name": "Raj Koothrappali",
+        "photo": "",
+        "scores": [
+            3, 5, 1, 3, 2, 2, 3, 4, 4, 5
+        ]
+    },
+
+    {
+        "name": "Sheldon Cooper",
+        "photo": "",
+        "scores": [
+            1, 2, 1, 3, 1, 1, 1, 5, 5, 1
+        ]
+    },
+
+    {
+        "name": "Amy Farrah Fowler",
+        "photo": "",
+        "scores": [
+            3, 3, 1, 2, 5, 4, 1, 5, 3, 2
+        ]
+    },
+
+    {
+        "name": "Bernadette Rostenkowski",
+        "photo": "",
+        "scores": [
+            3, 1, 2, 1, 3, 1, 3,  5, 5, 1
         ]
     }
 ];
--- FriendFinder/app/routing/apiRoutes.js
+++ FriendFinder/app/routing/apiRoutes.js
@@ -8,29 +8,35 @@ module.exports = function(app) {
 
 
     app.post("/api/friends", function(req, res) {
+
+        var newFriend = req.body;
+        var diffArr = [];
+
+        friendData.forEach(function(data) {
+            
+            for (var i = 0; i < newFriend.scores.length; i++) {
+                var existing = data.scores[i];
+                var newScore = newFriend.scores[i];
+                var difference = Math.abs(existing - newScore);
+            }
+
+            diffArr.push(difference);
+        });
+
+        var minDiff = Math.min.apply(null, diffArr);
+
+        var arrNum = diffArr.indexOf(minDiff);
+
+        var bestMatch = friendData[arrNum];
+
+
+        res.json(bestMatch);
+        console.log(bestMatch);
+
+        
         friendData.push(req.body);
         console.log(req.body);
 
-        var scoreInput = req.body.scores
-        var bestMatch = 0;
-        var worstMatch = 0;
-        
-        for (var i = 0; i < friendData.length-1; i++) {
-            var apiScores = friendData[i].scores;
-            var totalDiff = 0;
-
-            for (var j = 0; j < scoreInput.length-1; j++) {
-                var diff = Math.abs(parseInt(scoreInput[j]) - parseInt(apiScores[j]));
-                totalDiff = totalDiff + diff;
-            }
-
-            if (totalDiff < worstMatch) {
-                worstMatch = totalDiff;
-                bestMatch = i;
-            }
-        }
-
-        res.send(friendData[bestMatch]);
     
 });
 
